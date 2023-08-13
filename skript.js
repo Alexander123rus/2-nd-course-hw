c4419f6ab1365c3c20807d498e575e14203cbafd
Alexander123rus <141820790+Alexander123rus@users.noreply.github.com>
2023-08-13 17:08:22 +0300

    addea file stile.css

---
+++ skript.js
@@ -0,0 +1,53 @@
+// задание 1
+alert(10);
+alert(20);
+// задание 2
+alert(2007);
+//задание 3
+let message = 'Brendan Eich';
+alert(message); 
+//задание 4
+let $a = 10; 
+let $b = 2; 
+
+alert($a + $b); // 3
+alert($a + $b);
+alert($a - $b);
+alert($a * $b);
+alert($a / $b);
+//задание 5
+let $d = 2;
+let $c = 5;
+alert($d ** $c);
+// задание 6
+let $i = 9;
+let $u = 2;
+alert($i / $u);
+//задание7
+let num = 1;
+num =++ 5;
+num =-- 3;
+num = num * 7;
+num = num / 3;
+num = ++ 1;
+num = -- 1;
+alert(num);
+//задание 8
+let age =prompt("Сколько вам лет?")
+alert(age)
+//задание 9
+let user = {
+    name: "Alex",
+    years: 33
+  };
+  
+  //let {name, years: ages, isAdmin = false} = user;
+  alert( user.name );
+  alert( user.ages );
+ 
+  name=prompt("Какую информацию хотите узнать о пользователе?")
+ 
+ 
+ let name = prompt('Ваше имя?');
+ alert('Привет: ' + name);
+
